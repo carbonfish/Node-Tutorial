5d1401c86bd4eba6c3a88e9d293f3161f18b0478
cabonfish <pengyu1104@gmail.com>
2014-11-27 14:52:42 +0000

    add integration with jenkins

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function(grunt) {
     mochaTest: {
      test: { 
        options: { 
-         reporter: 'XUnit',
+         reporter: 'xunit',
          captureFile: 'test/report/result.xml'
        },
        src: ['test/**/*.js ']
